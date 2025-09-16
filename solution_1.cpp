861d7e9e2153ce1be7d43c290231f60d6e41468c
vlad-cool <vlad-matveev2004@mail.ru>
2025-09-16 15:52:08 +0300

    task 1 done

--- solution_1.cpp
+++ solution_1.cpp
@@ -4,17 +4,58 @@
 int main()
 {
     int arg = 0;
-    scanf("%d", &arg);
     int res = 0;
-    for (int i = 0; i < arg; i++)
+
+    int i = 0;
+    int j = 0;
+
+    int tmp = 0;
+
+    bool main_loop_cond = false;
+    bool main_loop_break_cond = false;
+    bool inner_loop_cond = false;
+
+    scanf("%d", &arg);
+
+main_loop:
+    main_loop_cond = i < arg;
+    main_loop_break_cond = !main_loop_cond;
+
+    if (main_loop_break_cond)
     {
-        int j = 1;
-        do
-        {
-            res += i % j;
-            j++;
-        } while (j < i);
+        goto main_loop_end;
     }
+    else
+    {
+        goto main_loop_body;
+    }
+
+main_loop_body:
+
+    j = 1;
+
+inner_loop:
+    tmp = i % j;
+    res = res + tmp;
+    j = j + 1;
+
+    inner_loop_cond = j < i;
+
+    if (inner_loop_cond)
+    {
+        goto inner_loop;
+    }
+    else
+    {
+        goto inner_loop_end;
+    }
+
+inner_loop_end:
+
+    i = i + 1;
+    goto main_loop;
+main_loop_end:
+
     printf("%d\n", res);
     return 0;
 }
